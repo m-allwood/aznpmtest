9705723058bf94b1efa20ebe8da1eb732315757f
m-allwood <89143088+m-allwood@users.noreply.github.com>
2023-05-23 10:11:27 -0400

    Update app.js
    Removed apostrophes in html body text

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><title>Matt Allwood's Website</title></body></html>');
+  res.end('<html><body><title>Matt Allwoods Website</title></body></html>');
 });
 
 server.listen(port, hostname, () => {
