d50112a23158f85f3fe908ee022176a1fcc25a3b
m-allwood <89143088+m-allwood@users.noreply.github.com>
2023-05-24 12:49:00 -0400

    Load html file into variable

--- html.js
+++ html.js
@@ -4,19 +4,25 @@ const fs = require('fs').promises;
 const hostname = '0.0.0.0';
 const port = 80;
 
+let indexFile;
+
 const server = http.createServer((req, res) => {
-  fs.readFile(__dirname + "/index.html")
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    res.end(indexFile);
+});
+
+fs.readFile(__dirname + "/index.html")
     .then(contents => {
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(200);
-        res.end(contents);
+        indexFile = contents;
+        server.listen(port, hostname, () => {
+            console.log('Server running at http://${hostname}:${port}/');
+        })
     })
     .catch(err => {
-        res.writeHead(500);
-        res.end(err);
-        return;
+        res.error('Could not read index.html file: ${err}');
+        process.exit(1);
     })
-});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
