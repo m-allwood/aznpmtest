3429621e44081fc820abc71f9cfb6bce2b24b581
m-allwood <89143088+m-allwood@users.noreply.github.com>
2023-05-25 10:36:58 -0400

    added json js file/library

---
+++ routes.js
@@ -0,0 +1,9 @@
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
