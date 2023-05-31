9cfe17b8dc0e6f362e9e703a425af395ce9f7ba4
m-allwood <89143088+m-allwood@users.noreply.github.com>
2023-05-31 09:21:37 -0400

    added routes.js

--- routes.js
+++ routes.js
@@ -7,3 +7,20 @@ const authors = JSON.stringify([
     { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
     { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
 ]);
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/books":
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}))
+    }
+}
